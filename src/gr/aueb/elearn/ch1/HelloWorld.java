e5a22013d9ec6f08580bf3a9879fa3b08d432d0d
Anomerianakis <anomerpan200@gmail.com>
2024-03-04 14:33:35 +0200

    Hello World v 0.2

--- src/gr/aueb/elearn/ch1/HelloWorld.java
+++ src/gr/aueb/elearn/ch1/HelloWorld.java
@@ -1,5 +1,9 @@
 package gr.aueb.elearn.ch1;
 
+/**
+ * Prints Hello World
+ * @author P. Anomerianakis
+ */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
